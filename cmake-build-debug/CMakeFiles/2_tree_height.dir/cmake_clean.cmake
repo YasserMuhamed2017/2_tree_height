ffc4bc50d335c49c5a7325185e43afea7334478b
Yasser Mohamed <yassermano848@gmail.com>
2020-05-05 11:26:13 +0200

    Initial Commit: simple code

---
+++ cmake-build-debug/CMakeFiles/2_tree_height.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_tree_height.exe"
+  "2_tree_height.exe.manifest"
+  "2_tree_height.pdb"
+  "CMakeFiles/2_tree_height.dir/main.cpp.obj"
+  "lib2_tree_height.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_tree_height.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
